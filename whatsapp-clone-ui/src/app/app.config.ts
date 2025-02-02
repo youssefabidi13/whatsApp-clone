13053168de657c644906855b28ed929d340abbdb
unknown <youssefabidi929@gmail.com>
2025-02-02 20:59:20 +0000

    bugfix: global issue js

--- whatsapp-clone-ui/src/app/app.config.ts
+++ whatsapp-clone-ui/src/app/app.config.ts
@@ -1,10 +1,19 @@
-import {ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection} from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {KeycloakService} from './utils/keycloak/keycloak.service';
 import {keycloakHttpInterceptor} from './utils/http/keycloak-http.interceptor';
+import {KeycloakService} from './utils/keycloak/keycloak.service';
+
+export function kcFactory(kcService: KeycloakService) {
+  return () => kcService.init();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
